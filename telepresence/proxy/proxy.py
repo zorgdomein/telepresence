994800c37161f63ba9e709b353cc91806e62906c
Abhay Saxena <ark3@datawire.io>
2020-06-25 22:20:34 -0400

    proxy: Use a proxy Pod for the "new deployment" operation

--- telepresence/proxy/proxy.py
+++ telepresence/proxy/proxy.py
@@ -1,4 +1,4 @@
-# Copyright 2018 Datawire. All rights reserved.
+# Copyright 2020 Datawire. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,19 +11,26 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 import argparse
+import json
 from subprocess import CalledProcessError
-from typing import Callable, NamedTuple, Optional, Tuple, Type
+from typing import Callable, List, NamedTuple, Optional, Tuple, Type
 
 from telepresence.cli import PortMapping
 from telepresence.runner import Runner
 from telepresence.utilities import get_alternate_nameserver
 
 from .deployment import (
-    create_new_deployment, existing_deployment, existing_deployment_openshift,
+    existing_deployment, existing_deployment_openshift, get_image_name,
     supplant_deployment, swap_deployment_openshift
 )
-from .remote import RemoteInfo, get_remote_info
+from .manifest import (
+    Manifest, make_k8s_list, make_new_proxy_pod_manifest, make_svc_manifest
+)
+from .remote import (
+    RemoteInfo, get_remote_info, make_remote_info_from_pod, wait_for_pod
+)
 
 ProxyIntent = NamedTuple(
     "ProxyIntent", [
@@ -150,6 +157,7 @@ LegacyOperation = Callable[[Runner, str, PortMapping, Optional[str], str],
 class ProxyOperation:
     def __init__(self, intent: ProxyIntent) -> None:
         self.intent = intent
+        self.remote_info = None  # type: Optional[RemoteInfo]
 
     def prepare(self, runner: Runner) -> None:
         pass
@@ -186,8 +194,77 @@ class ProxyOperation:
 
 
 class New(ProxyOperation):
+    def prepare(self, runner: Runner) -> None:
+        self.manifests = []  # type: List[Manifest]
+
+        # Construct a Pod manifest
+        env = {}
+        if self.intent.custom_nameserver:
+            # If we're on local VM we need to use different nameserver to
+            # prevent infinite loops caused by sshuttle:
+            env["TELEPRESENCE_NAMESERVER"] = self.intent.custom_nameserver
+
+        pod = make_new_proxy_pod_manifest(
+            self.intent.name,
+            runner.session_id,
+            get_image_name(runner, self.intent.expose),
+            self.intent.service_account,
+            env,
+        )
+        self.manifests.append(pod)
+
+        # Construct a Service manifest as needed
+        if self.intent.expose.remote():
+            svc = make_svc_manifest(
+                self.intent.name,
+                dict(telepresence=runner.session_id),
+                dict(telepresence=runner.session_id),
+                {p: p
+                 for p in self.intent.expose.remote()},
+            )
+            self.manifests.append(svc)
+
+        self.remote_info = make_remote_info_from_pod(pod)
+
     def act(self, runner: Runner) -> RemoteInfo:
-        return self._legacy(runner, create_new_deployment, "deployment")
+        assert self.remote_info is not None
+
+        runner.show(
+            "Starting network proxy to cluster using "
+            "new Pod {}".format(self.intent.name)
+        )
+
+        manifest_list = make_k8s_list(self.manifests)
+        manifest_json = json.dumps(manifest_list)
+        try:
+            runner.check_call(
+                runner.kubectl("create", "-f", "-"),
+                input=manifest_json.encode("utf-8")
+            )
+        except CalledProcessError as exc:
+            raise runner.fail(
+                "Failed to create Pod/Service {}:\n{}".format(
+                    self.intent.name, exc.stderr
+                )
+            )
+
+        def clean_up():
+            runner.show("Cleaning up Pod/Service {}".format(self.intent.name))
+            runner.check_call(
+                runner.kubectl(
+                    "delete",
+                    "--ignore-not-found",
+                    "--wait=false",
+                    "--selector=telepresence=" + runner.session_id,
+                    "svc,pod",
+                )
+            )
+
+        runner.add_cleanup("Delete new Pod/Service", clean_up)
+
+        wait_for_pod(runner, self.remote_info)
+
+        return self.remote_info
 
 
 class ExistingDeploy(ProxyOperation):
@@ -212,3 +289,62 @@ class SwapDC(ProxyOperation):
         return self._legacy(
             runner, swap_deployment_openshift, "deploymentconfig"
         )
+
+
+"""
+class Swap(ProxyOperation):
+    def prepare(self, runner: Runner) -> None:
+        # Grab original deployment's Pod Config
+        deployment = get_deployment(runner, name)  # from .remote
+
+        # Compute proxy Pod's manifest
+        pod_spec = deployment["spec"]["template"]["spec"]
+        # TODO: perform the usual swap changes
+        # TODO: rip off from new_swapped_deployment(...)
+        # FIXME: Implement this...
+
+        # FIXME: Copy-pasta from New.prepare(...)
+        # FIXME: factor out more of making a Tel pod?
+        pod = make_new_proxy_pod_manifest(...)
+
+        self.remote_info = make_remote_info_from_pod(pod)
+
+    def act(self, runner: Runner) -> RemoteInfo:
+        assert self.remote_info is not None
+
+        # FIXME: Copy-pasta from New.act(...)
+        # Apply the manifest
+        # Set up for cleanup
+
+        # FIXME: Factor this out?
+        # This all seems repetitive
+
+        wait_for_pod(runner, self.remote_info)
+
+        return self.remote_info
+
+
+class Existing(ProxyOperation):
+    def prepare(self, runner: Runner) -> None:
+        # Grab original Deployment's manifest
+        deployment = get_deployment(runner, name)  # from .remote
+        deployment_name = deployment["metadata"]["name"]  # type: str
+        deployment_type = deployment["kind"]  # type: str
+
+        # Find the Pod for this Deployment
+        # FIXME: Implement this
+        # TODO: This really does too much work; simplify it!
+        # E.g., this waits for the pod, which we don't want to do so early...
+        self.remote_info = get_remote_info(
+            runner, deployment_name, deployment_type, runner.session_id
+        )
+
+    def act(self, runner: Runner) -> RemoteInfo:
+        assert self.remote_info is not None
+
+        # Nothing to do here, right?
+
+        wait_for_pod(runner, self.remote_info)
+
+        return self.remote_info
+"""
