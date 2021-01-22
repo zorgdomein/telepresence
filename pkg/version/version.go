7cbabdcfcce61e6b1996c4ca61eb8c84a1531fa9
Luke Shumaker <lukeshu@datawire.io>
2021-01-22 11:03:04 -0700

    wip
    Signed-off-by: Luke Shumaker <lukeshu@datawire.io>

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -9,40 +9,46 @@ import (
 	"github.com/blang/semver"
 )
 
-// Version is a "vSEMVER" string, and is either populated at build-time using `--ldflags -X`, or at
-// init()-time by inspecting the binary's own debug info.
-var Version string
+// RawVersion is a "vSEMVER" string, and is either populated at build-time using `--ldflags -X`, or
+// at init()-time by inspecting the binary's own debug info.
+//
+// You should probably avoid using this, and instead use onf of the accessor methods.
+var RawVersion string
 
-// Semver is a parsed version of the Version string.
-var Semver semver.Version
+// parseVersion is a parsed version of the RawVersion string.
+var parsedVersion semver.Version
+
+
+func Semver() semver.Version { return semver }
+func String() semver.Version { return RawVersion }
+func GitTag() string         { return RawVersion }
+func DockerTag() string      { return RawVersion }
 
 func init() {
 	// Prefer version number inserted at build using --ldflags, but if it's not set...
-	if Version == "" {
-		if v := os.Getenv("TELEPRESENCE_VERSION"); v != "" {
-			Version = v
+	if RawVersion == "" {
+		if v := os.Getenv("TELEPRESENCE_VERSION"); strings.HasPrefix(v, "v") {
+			RawVersion = v
 		} else if i, ok := debug.ReadBuildInfo(); ok {
 			// Fall back to version info from "go get"
-			Version = i.Main.Version
+			RawVersion = i.Main.Version
 		} else {
-			Version = "(unknown version)"
+			RawVersion = "(unknown version)"
 		}
 	}
 
-	if (Semver == semver.Version{}) {
-		switch {
-		case strings.HasPrefix(Version, "("):
-			// Could be "(unknown version)" from above, or "(devel)" from
-			// debug.ReadBuildInfo(), and so lets go ahead and just allow all
-			// parenthesized strings as special cases.
-		case strings.HasPrefix(Version, "v"):
-			parsed, err := semver.Parse(Version[1:])
-			if err != nil {
-				panic(fmt.Errorf("binary compiled with invalid version; compiled-in version must be a %q string: %q: %w", "vSEMVER", Version, err))
-			}
-			Semver = parsed
-		default:
-			panic(fmt.Errorf("binary compiled with invalid version; compiled-in version must be a %q string: %q: missing \"v\" prefix", "vSEMVER", Version))
+	switch {
+	case strings.HasPrefix(RawVersion, "("):
+		// Could be "(unknown version)" from above, or "(devel)" from
+		// debug.ReadBuildInfo(), and so lets go ahead and just allow all
+		// parenthesized strings as special cases.
+	case strings.HasPrefix(RawVersion, "v"):
+		parsed, err := semver.Parse(RawVersion[1:])
+		if err != nil {
+			panic(fmt.Errorf("binary compiled with invalid version; compiled-in version must be a %q string: %q: %w", "vSEMVER", Version, err))
 		}
+		Semver = parsed
+	default:
+		panic(fmt.Errorf("binary compiled with invalid version; compiled-in version must be a %q string: %q: missing \"v\" prefix", "vSEMVER", Version))
 	}
 }
